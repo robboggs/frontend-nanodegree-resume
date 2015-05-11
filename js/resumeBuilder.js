2938eeabd020435ad852287bcce7710a971e0357
robboggs <robwboggs@gmail.com>
2015-05-11 12:47:19 -0700

    Finished lesson 1

---
+++ js/resumeBuilder.js
@@ -0,0 +1,83 @@
+
+// creating an object called bio with key value pairs
+var bio = {
+	"name" : "Robert Boggs",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "619-962-1635",
+		"email" : "robwoboggs@gmail.com",
+		"github" : "robboggs",
+		"twitter" : "jonbonman",
+		"location" : "San Francisco"
+	},
+	"welcomeMessage" : "Thanks for giving me this oppertunity",
+	"skills" : ["awesomesness", "python programming", "third item"],
+	"bioPic" : "images/fry.jpg"
+}
+
+// var formattedName = HTMLheaderName.replace(
+// 	"%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace(
+// 	"%data%", bio.role);
+// var formattedMobile = HTMLmobile.replace(
+// 	"%data%", bio.contacts);
+
+// $("#main").prepend(formattedName, 
+// 				   formattedRole,
+// 				   formattedMobile);
+//$("#main").prepend([formattedRole]);
+
+//var skills = 
+//["awesomesness", "python programming", "third item"];
+
+//$("#main").append(skills);
+
+
+var education = {
+	"schools" : [
+		{
+			"name" : "Southwestern",
+			"city" : "Chula Vista",
+			"degree" : "AA",
+			"major" : "General Studies"
+		},
+		{
+			"name" : "San Diego State University",
+			"city" : "San Diego",
+			"degree" : "BS",
+			"major" : "Geography"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "JavaScript Crash Course",
+			"school" : "Udacity",
+			"dates" : 2015,
+			"url" : "https://www.udacity.com/"
+		}
+	]
+}
+
+var work = {
+	"jobs" : [
+	{
+		"employer" : "company Name",
+		"title" : " binary worker",
+		"dates" : " beginning date - ending date",
+		"description" : "write what it is you did there here"
+	}
+	]
+}
+var projects = {
+	"projects" : [
+		{
+			"title" : "project title",
+			"dates" : " project dates",
+			"description" : "write what it is you did there here",
+			"images" : [
+				"hlkjashg iausghalgjkhlafugh",
+				"jlk;hiusahfvgkljablkuha"
+			]
+		}
+	]
+}
